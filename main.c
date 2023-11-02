5c18ab02041be84fb28dc7a0b27e2c1445901568
Jesse Guenzl <contact@jesseguenzl.com>
2023-11-02 21:18:37 +0200

    add FreeRTOS to project

--- main.c
+++ main.c
@@ -4,6 +4,10 @@
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
 
+#include "FreeRTOS.h"
+#include "FreeRTOSConfig.h"
+#include "task.h"
+
 #include "display/ssd1306.h"
 #include "display/image.h"
 #include "display/acme_5_outlines_font.h"
@@ -11,13 +15,14 @@
 #include "display/crackers_font.h"
 #include "display/BMSPA_font.h"
 
-const uint8_t num_chars_per_disp[]={7,7,7,5};
-const uint8_t *fonts[4]= {acme_font, bubblesstandard_font, crackers_font, BMSPA_font};
+const uint8_t num_chars_per_disp[] = {7, 7, 7, 5};
+const uint8_t *fonts[4] = {acme_font, bubblesstandard_font, crackers_font, BMSPA_font};
 
 #define SLEEPTIME 25
 
 void setup_gpios(void);
-void animation(void);
+
+void animationTask(void *param);
 
 int main() {
     stdio_init_all();
@@ -25,8 +30,18 @@ int main() {
     printf("configuring pins...\n");
     setup_gpios();
 
-    printf("jumping to animation...\n");
-    animation();
+    printf("jumping to animationTask...\n");
+
+    TaskHandle_t animationTaskHandle = NULL;
+    xTaskCreate(animationTask,
+                "animationTask",
+                1024,
+                NULL,
+                tskIDLE_PRIORITY + 1,
+                &animationTaskHandle
+                );
+
+    vTaskStartScheduler();
 
     return 0;
 }
@@ -40,12 +55,11 @@ void setup_gpios(void) {
     gpio_pull_up(15);
 }
 
-
-void animation(void) {
-    const char *words[]= {"SSD1306", "DISPLAY", "DRIVER"};
+void animationTask(void *param) {
+    const char *words[] = {"SSD1306", "DISPLAY", "DRIVER"};
 
     ssd1306_t disp;
-    disp.external_vcc=false;
+    disp.external_vcc = false;
     ssd1306_init(&disp, 128, 64, 0x3C, i2c1);
     ssd1306_clear(&disp);
 
@@ -53,57 +67,57 @@ void animation(void) {
 
     char buf[8];
 
-    for(;;) {
-        for(int y=0; y<31; ++y) {
+    for (;;) {
+        for (int y = 0; y < 31; ++y) {
             ssd1306_draw_line(&disp, 0, y, 127, y);
             ssd1306_show(&disp);
-            sleep_ms(SLEEPTIME);
+            vTaskDelay(SLEEPTIME);
             ssd1306_clear(&disp);
         }
 
-        for(int y=0, i=1; y>=0; y+=i) {
-            ssd1306_draw_line(&disp, 0, 31-y, 127, 31+y);
-            ssd1306_draw_line(&disp, 0, 31+y, 127, 31-y);
+        for (int y = 0, i = 1; y >= 0; y += i) {
+            ssd1306_draw_line(&disp, 0, 31 - y, 127, 31 + y);
+            ssd1306_draw_line(&disp, 0, 31 + y, 127, 31 - y);
             ssd1306_show(&disp);
-            sleep_ms(SLEEPTIME);
+            vTaskDelay(SLEEPTIME);
             ssd1306_clear(&disp);
-            if(y==32) i=-1;
+            if (y == 32) i = -1;
         }
 
-        for(int i=0; i<sizeof(words)/sizeof(char *); ++i) {
+        for (int i = 0; i < sizeof(words) / sizeof(char *); ++i) {
             ssd1306_draw_string(&disp, 8, 24, 2, words[i]);
             ssd1306_show(&disp);
-            sleep_ms(800);
+            vTaskDelay(800);
             ssd1306_clear(&disp);
         }
 
-        for(int y=31; y<63; ++y) {
+        for (int y = 31; y < 63; ++y) {
             ssd1306_draw_line(&disp, 0, y, 127, y);
             ssd1306_show(&disp);
-            sleep_ms(SLEEPTIME);
+            vTaskDelay(SLEEPTIME);
             ssd1306_clear(&disp);
         }
 
-        for(size_t font_i=0; font_i<sizeof(fonts)/sizeof(fonts[0]); ++font_i) {
-            uint8_t c=32;
-            while(c<=126) {
-                uint8_t i=0;
-                for(; i<num_chars_per_disp[font_i]; ++i) {
-                    if(c>126)
+        for (size_t font_i = 0; font_i < sizeof(fonts) / sizeof(fonts[0]); ++font_i) {
+            uint8_t c = 32;
+            while (c <= 126) {
+                uint8_t i = 0;
+                for (; i < num_chars_per_disp[font_i]; ++i) {
+                    if (c > 126)
                         break;
-                    buf[i]=c++;
+                    buf[i] = c++;
                 }
-                buf[i]=0;
+                buf[i] = 0;
 
                 ssd1306_draw_string_with_font(&disp, 8, 24, 2, fonts[font_i], buf);
                 ssd1306_show(&disp);
-                sleep_ms(800);
+                vTaskDelay(800);
                 ssd1306_clear(&disp);
             }
         }
 
         ssd1306_bmp_show_image(&disp, image_data, image_size);
         ssd1306_show(&disp);
-        sleep_ms(2000);
+        vTaskDelay(2000);
     }
 }
